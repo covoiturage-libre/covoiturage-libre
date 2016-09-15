6e738d2c0f88eec15a4ab8a0db36a89d9975f8fb
Thomas Blumenfeld <thomas@tymate.com>
2016-09-15 15:13:49 +0200

    homepage and twitter boostrap example layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
 end
