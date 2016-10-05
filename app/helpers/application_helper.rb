9c0401401c599754c8cefb31321170410ec1c885
Thomas Blumenfeld <thomas@tymate.com>
2016-10-05 09:48:49 +0200

    work on show trip (i18n, style and encode_decode)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     "#{age} ans".html_safe
   end
 
+  def encode_decode(string)
+    string.encode("iso-8859-1").force_encoding("utf-8") unless string.nil?
+  end
+  alias :ed :encode_decode
+
 end
