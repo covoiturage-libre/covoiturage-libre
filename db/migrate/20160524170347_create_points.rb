f3102252c612814db697c75b6493549efce31183
Thomas Blumenfeld <thomas@tymate.com>
2016-10-06 18:22:56 +0200

    bug

--- db/migrate/20160524170347_create_points.rb
+++ db/migrate/20160524170347_create_points.rb
@@ -4,8 +4,8 @@ class CreatePoints < ActiveRecord::Migration[5.0]
       t.string :kind
       t.integer :rank
       t.references :trip, foreign_key: true
-      t.decimal :lat, :decimal, precision: 9, scale: 6
-      t.decimal :lon, :decimal, precision: 9, scale: 6
+      t.decimal :lat, precision: 9, scale: 6
+      t.decimal :lon, precision: 9, scale: 6
       t.string :address1
       t.string :address2
       t.string :city
