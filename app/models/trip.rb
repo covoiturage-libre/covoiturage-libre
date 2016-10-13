08f61404c919d0cdd7a40738adb2c03187439fc4
Thomas Blumenfeld <thomas@tymate.com>
2016-10-13 18:52:15 +0200

    feature extraction

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 require 'elasticsearch/model'
 
 class Trip < ApplicationRecord
+  include Searchable
 
   # use of this classification https://en.wikipedia.org/wiki/Hotel_rating
   CAR_RATINGS = %w(standard comfort first_class luxury)
@@ -46,117 +47,15 @@ class Trip < ApplicationRecord
     self.update_attribute(:state, 'confirmed')
   end
 
-  def confirmed?
-    self.state == 'confirmed'
-  end
-
   def soft_delete!
     self.update_attribute(:state, 'deleted')
   end
 
-
-  ### ELASTICSEARCH SECTION
-
-  ELASTICSEARCH_MAX_RESULTS = 25
-  ELASTICSEARCH_GEO_DISTANCE = "10km"
-
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-  include Elasticsearch::Model::Indexing
-
-  mappings do
-    indexes :seats,      type: 'integer'
-    indexes :comfort,    type: 'string'
-    indexes :state,      type: 'string'
-    indexes :leave_at,   type: 'date'
-    indexes :points, type: 'nested' do
-      indexes :kind,       type: 'string'
-      indexes :rank,       type: 'string'
-      indexes :location,   type: 'geo_point'
-      indexes :city,       type: 'string'
-    end
-  end
-
-  def as_indexed_json(options = {})
-    self.as_json(
-        { only: [:seats, :comfort, :state, :leave_at] }
-    ).merge(
-        points: points.as_json
-    )
-  end
-
-  #
-  # Trip.search({ from_coordinates: {}, to_coordinates: {}, date: Date })
-  #
-  def self.search(options = {})
-    options ||= {}
-
-    # empty search not allowed, for now
-    return nil if options.blank?
-
-    # define search definition
-    search_definition = {
-        query: {
-            bool: {
-                must: []
-            }
-        }
-    }
-
-    unless options.blank?
-      search_definition[:from] = 0
-      search_definition[:size] = ELASTICSEARCH_MAX_RESULTS
-    end
-
-    # only look for confirmed trips
-    search_definition[:query][:bool][:must] << { term: { state: "confirmed" } }
-
-    # root object criteria
-    if options[:date].present?
-      search_definition[:query][:bool][:must] << { range: { leave_at: { gte: options[:date].to_s } } }
-    end
-
-    # geo spatial query on nested document (point)
-    if options[:from_coordinates].present? && options[:to_coordinates].present?
-      search_definition[:query][:bool][:must] << nested_point_definition("From", options[:from_coordinates])
-      search_definition[:query][:bool][:must] << nested_point_definition("To", options[:to_coordinates])
-    end
-
-    # pretty log for json elasticsearch request (do not delete)
-    #logger.info JSON.pretty_generate search_definition
-
-    __elasticsearch__.search(search_definition)
+  def confirmed?
+    state == 'confirmed'
   end
 
   private
-
-    def self.nested_point_definition(kind, coordinates)
-      {
-          nested: {
-              path: :points,
-              query: {
-                  bool: {
-                    must: [
-                        {
-                            match: {
-                              'points.kind': kind
-                            }
-                        },
-                        {
-                            geo_distance: {
-                              distance: ELASTICSEARCH_GEO_DISTANCE,
-                              'points.location': {
-                                lat: coordinates[:lat],
-                                lon: coordinates[:lon]
-                              }
-                            }
-                        }
-                    ]
-                  }
-              }
-          }
-      }
-    end
 
     def must_have_from_and_to_points
       logger.info errors.full_messages
