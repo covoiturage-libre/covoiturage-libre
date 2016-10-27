043a5cd22a0e5c98dff29e75c2a0ce1e9307a258
Thomas Blumenfeld <thomas@tymate.com>
2016-10-27 11:59:54 +0200

    indexing location data for performance enhancement

---
+++ db/migrate/20161027090728_add_location_index_to_points.rb
@@ -0,0 +1,15 @@
+class AddLocationIndexToPoints < ActiveRecord::Migration[5.0]
+  def change
+    execute %{
+      create index index_on_points_location ON points using gist (
+        ST_GeographyFromText(
+          'SRID=4326;POINT(' || points.lon || ' ' || points.lat || ')'
+        )
+      )
+    }
+  end
+
+  def down
+    execute %{drop index index_on_points_location}
+  end
+end
