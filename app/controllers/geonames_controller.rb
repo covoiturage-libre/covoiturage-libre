4251114d9db817df5750f35c0929e34f51acc853
Thomas Blumenfeld <thomas@tymate.com>
2016-11-02 18:10:15 +0100

    limit searchick to 10

--- app/controllers/geonames_controller.rb
+++ app/controllers/geonames_controller.rb
@@ -1,7 +1,7 @@
 class GeonamesController < ApplicationController
 
   def autocomplete
-    @results = Geoname.search(params[:term])
+    @results = Geoname.search(params[:term], limit: 10)
   end
 
 end
