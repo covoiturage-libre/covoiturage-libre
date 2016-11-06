9fe6abc33ec940fa88e4c7b262a54ed78bdc720b
Thomas Blumenfeld <thomas@tymate.com>
2016-11-06 14:20:00 +0100

    remove trips_helper because of deployment LoadError pb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,15 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def trip_steps_breadcrumb(trip)
+    breadcrumbs = ''
+    trip.points.each_with_index do |step|
+      breadcrumbs << step.city
+      unless step == trip.points.last
+        breadcrumbs << ' &rarr; '
+      end
+    end
+    breadcrumbs.html_safe
+  end
+
 end
