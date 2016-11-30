90c617b1b671ef973d752ff351cd7623d55bb3ef
Thomas Blumenfeld <thomas@tymate.com>
2016-11-30 16:50:30 +0100

    dynamic error pages - solves #74

--- config/routes.rb
+++ config/routes.rb
@@ -42,5 +42,8 @@ Rails.application.routes.draw do
     app.generate(:text, params[:text], 'font-size': 14)
   }
 
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
   root to: 'home#index'
 end
