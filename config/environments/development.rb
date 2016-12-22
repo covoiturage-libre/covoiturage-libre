7a0faa2b003d0b9cefedb97bdd7c0338a4aeef76
Thomas Blumenfeld <thomas@tymate.com>
2016-12-22 17:54:23 +0100

    use a logger in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Dev Logger
+  config.logger = Logger.new(STDOUT)
+
   # Show full error reports.
   config.consider_all_requests_local = false
 
