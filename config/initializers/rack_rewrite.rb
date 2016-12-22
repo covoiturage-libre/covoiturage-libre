03eb9d9f2175a2df461554f8d0b9b273b343cca1
Thomas Blumenfeld <thomas@tymate.com>
2016-12-22 14:41:04 +0100

    redirect old dynamic routes

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -6,4 +6,12 @@ Rails.application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) 
   r301 '/faq.php', '/faq'
   r301 '/contact.php', '/contact'
   r301 '/mentions-legales.php', '/mentions-legales'
+
+  r301 '/nouveau.php', '/trajets/nouveau'
+
+  r301 %r{/detail\.php\?c=(\w+).*}, '/trajets/$1'
+  r301 %r{/validation\.php\?c=(\w+).*}, '/trajets/$1/confirm'
+  r301 %r{/modification\.php\?m=(\w+).*}, '/trajets/$1/edit'
+  r301 %r{/suppression\.php\?supp=(\w+).*}, '/trajets/$1/edit'
+
 end
