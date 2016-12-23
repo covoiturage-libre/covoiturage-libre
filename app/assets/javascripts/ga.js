91c40887b1c261fa08f6922f2840c1434cbddfb4
Thomas Blumenfeld <thomas@tymate.com>
2016-12-23 20:07:04 +0100

    use pathname only

--- app/assets/javascripts/ga.js
+++ app/assets/javascripts/ga.js
@@ -1,9 +1,10 @@
 // Googla Analytics PageView Tracking compatibility
 // see https://github.com/turbolinks/turbolinks/issues/73
+// also see : http://reed.github.io/turbolinks-compatibility/google_universal_analytics.html
+// Google Analytics ref here : https://developers.google.com/analytics/devguides/collection/analyticsjs/pages
 
 document.addEventListener("turbolinks:load", function(event) {
   if (typeof ga === "function") {
-    ga("set", "location", event.data.url);
-    return ga("send", "pageview");
+    return ga('send', 'pageview', location.pathname);
   }
 });
