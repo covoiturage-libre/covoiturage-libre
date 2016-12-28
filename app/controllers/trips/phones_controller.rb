2254fb253bc1313bf4412c82a35fc3427d175f55
Thomas Blumenfeld <thomas@tymate.com>
2016-12-28 13:26:24 +0100

    try to fix unknown format issue

--- app/controllers/trips/phones_controller.rb
+++ app/controllers/trips/phones_controller.rb
@@ -2,9 +2,6 @@ class Trips::PhonesController < ApplicationController
 
   def show
     @trip = Trip.find_by(confirmation_token: params[:trip_id])
-    respond_to do |format|
-      format.js
-    end
   end
 
 end
