c60512eb70277ad9bddfa90a0c8cdeac283daf4e
Glen <glen@mailoo.org>
2017-04-25 20:08:41 +0200

    validate email format before responding to announce

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,8 @@ class Message < ApplicationRecord
   belongs_to :trip
 
   validates_presence_of :trip, :sender_name, :sender_email, :body
-
+  validates_format_of :sender_email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   after_create :send_notification_email
 
   private
