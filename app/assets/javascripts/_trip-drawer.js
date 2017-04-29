204e2faf4a4fa5c17835813f9d92f0c4d5f7bb37
Glen <glen@mailoo.org>
2017-04-29 18:05:56 +0200

    reduce cyclomatic complexity and more semicolons

--- app/assets/javascripts/_trip-drawer.js
+++ app/assets/javascripts/_trip-drawer.js
@@ -4,27 +4,26 @@ function isFloat(n){
   return Number(n) === n && n % 1 !== 0;
 }
 
-function secondsToStringFR(seconds)
-{
-  var sentence = "";
+function numberAppendString(num, string) {
+  if (num > 0) {
+    return num + string;
+  }
+  return "";
+}
+
+function secondsToStringFR(seconds) {
   var numyears = Math.floor(seconds / 31536000);
   var numdays = Math.floor((seconds % 31536000) / 86400);
   var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
   var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
   var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
-
-  if (numyears > 0) {
-    sentence = sentence + numyears + " ans ";
-  }
-  if (numdays > 0) {
-    sentence = sentence + numdays + " jours ";
-  }
-  if (numhours > 0) {
-    sentence = sentence + numhours + " heures ";
-  }
-  if (numminutes > 0) {
-    sentence = sentence + numminutes + " minutes ";
-  }
+  
+  var sentence = "";
+  sentence = sentence + numberAppendString(numyears, " ans ");
+  sentence = sentence + numberAppendString(numdays, " jours ");
+  sentence = sentence + numberAppendString(numhours, " heures ");
+  sentence = sentence + numberAppendString(numminutes, " minutes ");
+  
   return sentence;
 }
 
@@ -58,11 +57,11 @@ var TripDrawing = function() {
     self.observeGeonameChanges();
     self.manageCocoonEvents();
     self.observeAndGetRouteInfo();
-  }
+  };
 
   self.observeGeonameChanges = function() {
     $(".trip_points_lon input").change(self.updateOrCreatePoint);
-  }
+  };
 
   self.manageCocoonEvents = function() {
     $("#steps")
@@ -77,7 +76,7 @@ var TripDrawing = function() {
         self.removePointAtIndex(self.deletedRank);
         self.reorderSteps();
       });
-  }
+  };
 
   self.updateOrCreatePoint = function() {
     var lon = parseFloat($(this).val());
@@ -87,7 +86,7 @@ var TripDrawing = function() {
     var point = new Point(lat, lon, rank, kind);
     self.points[point.rank] = point.arrayValue();
     self.renderRouting();
-  }
+  };
 
   self.removePointAtIndex = function(index) {
     if (index === self.maxRank ) {
@@ -96,7 +95,7 @@ var TripDrawing = function() {
       self.points.splice(index, 1);
     }
     self.renderRouting();
-  }
+  };
 
   self.reorderSteps = function() {
     $("#steps .nested-fields").each(function(index, value) {
@@ -104,14 +103,14 @@ var TripDrawing = function() {
       $(this).find(".trip_points_city label:first").text("Étape "+ newIndex);
       $(this).find(".trip_points_rank input:first").val(newIndex);
     });
-  }
+  };
 
   // rendering and routing methods
 
   self.renderRouting = function() {
     self.routing.setWaypoints(self.cloneAndTrimArray());
     self.routing.route();
-  }
+  };
 
   self.observeAndGetRouteInfo = function() {
     self.routing.on("routesfound", function (e) {
@@ -121,20 +120,20 @@ var TripDrawing = function() {
       $("#trip_total_time").val(self.totalTime);
       $("#distance_and_time").text(metersToKmString(self.totalDistance) + " km - " + secondsToStringFR(self.totalTime));
     });
-  }
+  };
 
   // more or less "private" methods
 
   self.cloneAndTrimArray = function() {
     var copiedArray = self.points.slice(0);
     return copiedArray.filter(function(val) { return val !== null; });
-  }
+  };
 
   self.hasAFirstLatLon = function(anArray) {
     var result = true;
     if (anArray[0][0] === 0) { result = false; }
     if (anArray[0][1] === 0) { result = false; }
     return result;
-  }
+  };
 
 };
