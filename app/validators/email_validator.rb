7d08016650f40f4d11c5d1bab33c2f3f0e7f538c
Thomas Blumenfeld <thomas@tymate.com>
2017-05-16 15:42:33 +0200

    just a wording change

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,7 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "n'est pas un email")
+      record.errors[attribute] << (options[:message] || "n'est pas un email valide")
     end
   end
 end
