7e3e26883b7dc03ec0059f0b42d00172a666e093
Glen <glen@mailoo.org>
2017-08-18 18:36:35 +0200

    step prices MUST be in crescent order now

--- app/validators/prices_validator.rb
+++ app/validators/prices_validator.rb
@@ -1,11 +1,18 @@
 class PricesValidator < ActiveModel::Validator
   def validate(record)
-    puts "#############################################"
-    record.points.each do |p|
-      puts p["price"]
-      if p != "Evil"
+    # sort trip points by rank
+    sorted_points = record.points.sort_by { |aoh| aoh[:rank] }
+    # complete origin and destination prices
+    sorted_points[0]["price"] = 0
+    sorted_points[-1]["price"] = record.price
+    # check for growing values of points prices
+    i = 0
+    sorted_points.each do |p|
+      if p["price"] < i
         record.errors[:price] << "Les prix des étapes doivent être indiqués dans l\'ordre croissant."
+        break
       end
+      i = p["price"]
     end
   end
 end
