e6d205e34eb1eb0fc73959ca72138f9e61c921a5
djakk <djakk.djakk@gmail.com>
2017-10-04 17:24:38 +0200

    mispelled date -> today

--- app/models/search.rb
+++ app/models/search.rb
@@ -20,15 +20,16 @@ class Search
     # date en texte de type : "vendredi 13 mai 2011" : 4 chiffres -> l'année ; 1 ou 2 chiffres -> le jour ; le mois en lettres est dans I18n.t('date.month_names'), .index("mois en lettres") donne le mois en chiffres
     #begin
     #Date.strptime(self.date, '%d/%m/%Y')
-    the_match_object = /([A-Za-z]+) ([0-9]{1,2}) ([A-Za-z]+) ([0-9]{4})/.match(self.date)
-    the_day_in_digits = Integer(the_match_object[2])
-    the_month_in_text = the_match_object[3].downcase
-    the_year_in_digits = Integer(the_match_object[4])
-    the_month_in_digits = I18n.t('date.month_names').index(the_month_in_text)
-    Date.new(the_year_in_digits, the_month_in_digits, the_day_in_digits)
-    #rescue
-      #Date.today
-    #end
+    begin
+      the_match_object = /([A-Za-z]+)[ ]+([0-9]{1,2})[ ]+([A-Za-z]+)[ ]+([0-9]{4})/.match(self.date)
+      the_day_in_digits = Integer(the_match_object[2])
+      the_month_in_text = the_match_object[3].downcase
+      the_year_in_digits = Integer(the_match_object[4])
+      the_month_in_digits = I18n.t('date.month_names').index(the_month_in_text)
+      Date.new(the_year_in_digits, the_month_in_digits, the_day_in_digits)
+    rescue
+      Date.today
+    end
   end
 
   private
