4a5220b098056736894cbae5c5cc0bf9a1d990f0
Glen <glen@mailoo.org>
2017-10-19 23:38:41 +0200

    Fixed validation of emails with trailling spaces

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    unless value.strip =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
       record.errors[attribute] << (options[:message] || "n'est pas un email valide")
     end
   end
