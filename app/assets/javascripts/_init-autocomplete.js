f2e162d1f7a5a974c0ebedaf348bf44b2124db7a
Nicolas Leger <nicolas.leger@nleger.com>
2017-10-21 11:39:05 +0200

    Auto hide error tooltip when city selected

--- app/assets/javascripts/_init-autocomplete.js
+++ app/assets/javascripts/_init-autocomplete.js
@@ -25,6 +25,8 @@ jQuery.fn.extend({
         var lon = $("#" + this.id.replace(/city/, "lon"));
         lat.val(ui.item.lat).trigger("change");
         lon.val(ui.item.lon).trigger("change");
+        $(this).parent().removeClass('has-error');
+        $(this).next('.help-block').hide();
 
         if (typeof ga === "function") {
           ga("send", "event", "Ville", "select", ui.item.city);
@@ -57,6 +59,8 @@ jQuery.fn.extend({
           this.value = this.latestFocus.value;
           lat.val(this.latestFocus.lat).trigger("change");
           lon.val(this.latestFocus.lon).trigger("change");
+          $(this).parent().removeClass('has-error');
+          $(this).next('.help-block').hide();
 
           if (typeof ga === "function") {
             ga('send', 'event', 'Ville', 'select', this.latestFocus.city);
