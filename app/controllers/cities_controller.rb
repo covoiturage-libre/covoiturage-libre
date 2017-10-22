7b27e78ff315a231d94f519dff331bbc60aad88a
Glen <glen@mailoo.org>
2017-10-22 20:44:52 +0200

    some more cities as we make use of them now for boosting search

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,7 +5,6 @@ class CitiesController < ApplicationController
   MAIN_CITIES = %w(
     Paris
     Marseille
-    Bruxelles
     Lyon
     Toulouse
     Nice
@@ -13,6 +12,72 @@ class CitiesController < ApplicationController
     Strasbourg
     Montpellier
     Bordeaux
+    Lille
+    Rennes
+    Reims
+    Le Havre
+    Saint-Étienne
+    Toulon
+    Grenoble
+    Dijon
+    Nîmes
+    Angers
+    Villeurbanne
+    Saint-Denis
+    Le Mans
+    Aix-en-Provence
+    Clermont-Ferrand
+    Brest
+    Tours
+    Limoges
+    Amiens
+    Perpignan
+    Metz
+    Boulogne-Billancourt
+    Besançon
+    Orléans
+    Mulhouse
+    Saint-Denis
+    Rouen
+    Argenteuil
+    Caen
+    Montreuil
+    Saint-Paul
+    Nancy
+    Roubaix
+    Tourcoing
+    Nanterre
+    Avignon
+    Vitry-sur-Seine
+    Créteil
+    Dunkerque
+    Poitiers
+    Asnières-sur-Seine
+    Versailles
+    Courbevoie
+    Colombes
+    Fort-de-France
+    Aulnay-sous-Bois
+    Saint-Pierre
+    Cherbourg-en-Cotentin
+    Aubervilliers
+    Rueil-Malmaison
+    Pau
+    Le Tampon
+    Champigny-sur-Marne
+    Calais
+    Antibes
+    Béziers
+    Saint-Maur-des-Fossés
+    La Rochelle
+    Cannes
+    Saint-Nazaire
+    Mérignac
+    Drancy
+    Colmar
+    Ajaccio
+    Issy-les-Moulineaux
+    Bourges
   )
 
   def autocomplete
