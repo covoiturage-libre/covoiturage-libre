33c7957be684c73170205ac4c43640aa9eb80f52
Nicolas Leger <nicolas.leger@nleger.com>
2017-10-23 15:54:10 +0200

    Fix website URL in mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   add_template_helper(ApplicationHelper)
 
   SUBJECT_PREFIX = ENV['MAILER_SUBJECT_PREFIX'] || '[Covoiturage-libre.fr]'
-  
+
   def trip_confirmation(trip)
     @trip = trip
     mail(
@@ -38,9 +38,9 @@ class UserMailer < ApplicationMailer
       subject: prefix_subject('Vous avez envoyé un message')
     )
   end
-  
+
   private
-  
+
   def prefix_subject(subject)
     "#{SUBJECT_PREFIX} #{subject}"
   end
