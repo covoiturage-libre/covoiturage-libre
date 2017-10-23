799d10383cde62a5642e78e59317688d3c0ec328
Nicolas Leger <nicolas.leger@nleger.com>
2017-10-23 15:54:38 +0200

    Inherit mail default from env config

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,5 +2,8 @@
 Mailjet.configure do |config|
   config.api_key = ENV['MAILJET_API_KEY']
   config.secret_key = ENV['MAILJET_API_SECRET']
-  config.default_from = 'noreply@covoiturage-libre.fr'
+
+  mail_host = Rails.application.config.action_mailer.default_url_options[:host]
+  domain = URI.parse(mail_host).domain rescue 'covoiturage-libre.fr'
+  config.default_from = "noreply@#{domain}"
 end
