29f30593e0165337961a73cb9656d73a4992c7fe
Nicolas Leger <nicolasleger@users.noreply.github.com>
2017-11-03 18:15:06 +0100

    Revert "Fix/cities input (#215)"
    This reverts commit fcd7c8d919e0b6b324e3e8c873fee1b40c9e4c96.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,34 +1,14 @@
 class CitiesController < ApplicationController
 
-  # TOP 10 FR by population
-  MAIN_CITIES = %w(
-    Paris
-    Marseille
-    Bruxelles
-    Lyon
-    Toulouse
-    Nice
-    Nantes
-    Strasbourg
-    Montpellier
-    Bordeaux
-  )
-
   def autocomplete
     @results = City.search(
       params[:term],
       limit: 5,
-      boost_where: { country_code: { value: 'FR', factor: 5 } },
+      boost_where: {country_code: {value: 'FR', factor: 5}},
       fields: [:name, :postal_code],
       match: params[:term].to_i > 0 ? :word : :word_start,
       highlight: true
     )
-  end
-
-  def main
-    @results = City.where(name: MAIN_CITIES)
-                   .sort_by{ |city| MAIN_CITIES.index city.name }
-    render 'autocomplete'
   end
 
 end
