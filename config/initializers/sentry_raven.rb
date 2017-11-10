c4c621b4d6658564e0b64c1a4f9bc83b024684cd
Nicolas Leger <nicolas.leger@nleger.com>
2017-11-10 14:41:45 +0100

    Use conventional SENTRY_DSN instead of SENTRY_RAVEN_DSN

--- config/initializers/sentry_raven.rb
+++ config/initializers/sentry_raven.rb
@@ -1,6 +1,3 @@
-if ENV.has_key? 'SENTRY_RAVEN_DSN'
-  Raven.configure do |config|
-    config.dsn = ENV.fetch('SENTRY_RAVEN_DSN')
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  end
+Raven.configure do |config|
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
