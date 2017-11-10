f855d0a7d1a0cef1bb55860d2ddfaa74e5c39c30
Nicolas Leger <nicolasleger@users.noreply.github.com>
2017-11-10 14:27:17 +0100

    Allow console display for Docker (#247)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow console display for Docker
+  config.web_console.whiny_requests = false
 end
