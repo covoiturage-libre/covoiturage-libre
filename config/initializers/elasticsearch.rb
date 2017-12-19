fefc73b94d4f1ef4d02ff2d32bd0bbd9d2a44ee3
Nicolas Leger <nicolasleger@users.noreply.github.com>
2017-12-19 12:36:04 +0100

    Remove useless variable

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,4 +2,4 @@ require 'elasticsearch/transport'
 
 ENV['ELASTICSEARCH_URL'] ||= ENV['SCALINGO_ELASTICSEARCH_URL'] || ENV['SEARCHBOX_SSL_URL'] || ENV['SEARCHBOX_URL']
  
-client = Elasticsearch::Client.new transport_options: { ssl: { verify: false } }
+Elasticsearch::Client.new transport_options: { ssl: { verify: false } }
