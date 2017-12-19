4a20bd0105593c8cf326f8f618545f706112e89a
Nicolas Leger <nicolas.leger@nleger.com>
2017-12-19 12:12:11 +0100

    Fix ElasticSearch Client with SSL

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,6 @@
-require 'typhoeus/adapters/faraday'
+
+require 'elasticsearch/transport'
 
 ENV['ELASTICSEARCH_URL'] ||= ENV['SCALINGO_ELASTICSEARCH_URL'] || ENV['SEARCHBOX_SSL_URL'] || ENV['SEARCHBOX_URL']
+ 
+client = Elasticsearch::Client.new transport_options: { ssl: { verify: false } }
