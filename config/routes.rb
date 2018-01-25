bb22c79e53eefbe77561a6ba1630e1c4d5c31bf9
Nicolas Leger <nicolas.leger@nleger.com>
2018-01-25 18:44:43 +0100

    fix(285) Ensure autocomplete JSON format

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
 
   resources :cities do
     collection do
-      get 'autocomplete'
+      get 'autocomplete', defaults: { format: :json }
       get 'main'
     end
   end
