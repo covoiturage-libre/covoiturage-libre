e7e8f886a2d6a2abc42dee61fb4a08d7aa0a076b
Nicolas Leger <nicolas.leger@nleger.com>
2018-01-30 11:15:34 +0100

    fix(285) Add test on new Trip with wrong time

---
+++ spec/controllers/trips_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe TripsController, type: :controller do
+  describe "POST #create" do
+
+    it "should save new Trip with good time" do
+      expect{
+        post :create, trip: FactoryBot.attributes_for(:trip).except(:departure_time).merge(
+          "departure_time(1i)" => "2018",
+          "departure_time(2i)" => "1",
+          "departure_time(3i)" => "30",
+          "departure_time(4i)" => "16",
+          "departure_time(5i)" => "10"
+        )
+      }.to change(Trip, :count).by(1)
+    end
+
+    it "should render error when wrong time filled" do
+      expect{
+        post :create, trip: {
+          "departure_time(1i)" => "2018",
+          "departure_time(2i)" => "1",
+          "departure_time(3i)" => "30",
+          "departure_time(4i)" => "42",
+          "departure_time(5i)" => "10"
+        }
+      }.not_to raise_error(ActiveRecord::MultiparameterAssignmentErrors)
+      response.should render_template :new
+    end
+
+  end
+end
