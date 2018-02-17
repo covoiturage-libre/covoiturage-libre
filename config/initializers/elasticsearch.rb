5efaa9264253edf08c3b40481a279602821c296a
Nicolas Leger <nicolasleger@users.noreply.github.com>
2018-02-17 12:40:56 +0100

    Fix/increase elasticsearch timeout (#318)
    * Update README.md (#317)
    
    * Increase ElasticSearch timeout to 30s

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,8 @@
 require 'elasticsearch/transport'
 
 ENV['ELASTICSEARCH_URL'] ||= ENV['SCALINGO_ELASTICSEARCH_URL'] || ENV['SEARCHBOX_SSL_URL'] || ENV['SEARCHBOX_URL']
- 
-Elasticsearch::Client.new transport_options: { ssl: { verify: false } }
+
+Elasticsearch::Client.new transport_options: {
+  request_timeout: 30, # seconds
+  ssl: { verify: false }
+}
