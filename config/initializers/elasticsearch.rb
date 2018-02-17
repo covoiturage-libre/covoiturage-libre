eff0aded0caad2ffe726ec07972b71df30c7189e
Nicolas Leger <nicolas.leger@nleger.com>
2018-02-17 13:19:42 +0100

    Fix ElasticSearch timeout config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,6 +3,6 @@ require 'elasticsearch/transport'
 ENV['ELASTICSEARCH_URL'] ||= ENV['SCALINGO_ELASTICSEARCH_URL'] || ENV['SEARCHBOX_SSL_URL'] || ENV['SEARCHBOX_URL']
 
 Elasticsearch::Client.new transport_options: {
-  request: { open_timeout: 30 }, # seconds
+  request: { open_timeout: ENV.fetch('ELASTICSEARCH_TIMEOUT', 30).to_i }, # seconds
   ssl: { verify: false }
 }
