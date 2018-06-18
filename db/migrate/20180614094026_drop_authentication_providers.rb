2108384f02c57b7abe671fdbd62913af7242dbaa
Ghislain Loaec <gloaec@cadoles.com>
2018-06-18 10:05:40 +0200

    Damn it Travis

--- db/migrate/20180614094026_drop_authentication_providers.rb
+++
@@ -1,5 +0,0 @@
-class DropAuthenticationProviders < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :authentication_providers
-  end
-end
--- db/migrate/20180614094035_drop_user_authentications.rb
+++
@@ -1,5 +0,0 @@
-class DropUserAuthentications < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :user_authentications
-  end
-end
